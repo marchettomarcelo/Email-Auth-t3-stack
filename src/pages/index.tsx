c3a8d2f65659050811aaffcc041d696a22caa7f2
marchettomarcelo <marcelovfm@al.insper.edu.br>
2023-01-28 19:04:37 -0300

    acho que deu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
     button = (
       <button
         className=" rounded border-2 border-gray-200 py-4 px-8 text-lg font-bold shadow-2xl"
-        onClick={() => signOut()}
+        onClick={async () => await signOut()}
       >
         Sign Out
       </button>
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
     button = (
       <button
         className=" rounded border-2 border-gray-200 py-4 px-8 text-lg font-bold shadow-2xl"
-        onClick={() => signIn()}
+        onClick={async () => await signIn()}
       >
         Login
       </button>
