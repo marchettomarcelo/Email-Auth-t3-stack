04972fd844ca1b79131b99a163f576e3e39b39cb
marchettomarcelo <marcelovfm@al.insper.edu.br>
2023-02-01 09:43:17 -0300

    ajustes css

--- src/pages/pessoas.tsx
+++ src/pages/pessoas.tsx
@@ -1,21 +1,20 @@
 import { api } from "../utils/api";
 import Header from "../components/Header";
 import Link from "next/link";
+import BaseLayout from "../components/BaseLayout";
 
 function DiretorioMembros() {
   const pessoas = api.profile.getAllUsers.useQuery();
 
   return (
-    <main className="flex min-h-screen flex-col items-center gap-4">
-      <Header />
-
+    <BaseLayout>
       <h1 className="text-2xl font-bold">Todos os integrantes do gas:</h1>
       <div className="flex w-full flex-col items-center gap-4">
         {pessoas.data?.map((pessoa) => (
           <Link
             href={`/pessoas/${pessoa.username}`}
             key={pessoa.id}
-            className="w-3/5 rounded-md border p-4 shadow-md"
+            className=" w-full rounded-md border p-4 shadow-md"
           >
             <div className="flex w-full flex-col gap-1">
               <h2 className="text-xl font-bold">{pessoa.nome}</h2>
@@ -30,7 +29,7 @@ function DiretorioMembros() {
           </Link>
         ))}
       </div>
-    </main>
+    </BaseLayout>
   );
 }
 
